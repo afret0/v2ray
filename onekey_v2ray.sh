564f0e42acc8c81a007fe8a6e9e8349dbafc1783
afreto <ly.kong@neoclub.cn>
2020-08-07 18:00:40 +0800

    feat inputh doamin

--- onekey_v2ray.sh
+++ onekey_v2ray.sh
@@ -6,10 +6,13 @@
 
 
 
+read -p "your domain: " domain
+echo "your domain is "$domain
+
 wget -P /etc/v2ray https://raw.githubusercontent.com/afret0/v2ray/master/config.json
 wget -P /etc/caddy https://raw.githubusercontent.com/afret0/v2ray/master/Caddyfile
 
-domain="vultr.afreto.xyz"
+# domain="vultr.afreto.xyz"
 sed -i 's/domain.me/'${domain}'/g' /etc/caddy/Caddyfile 
 
 docker run -d --name caddy --restart always --net host -v /etc/caddy:/etc/caddy caddy
