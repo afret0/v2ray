02d8e6bc3ec31d7d0bb8aef613638d4688e84159
Afreto <kong5664546498@gmail.com>
2022-03-12 01:44:43 +0800

    feat add xray_web

--- onekey_v2ray.sh
+++ onekey_v2ray.sh
@@ -12,6 +12,19 @@ echo "your domain is "$domain
 wget -P /etc/v2ray https://raw.githubusercontent.com/afret0/v2ray/master/config.json
 wget -P /etc/caddy https://raw.githubusercontent.com/afret0/v2ray/master/Caddyfile
 
+if [ -x "$(command -v yum)" ]; then
+    echo ' use yum'>&2
+    yum install zip -y
+    yum install unzip -y
+fi
+
+if [ -x "$(command -v apt-get)" ]; then
+    echo 'use apt-get'>&2
+    apt-get install zip -y
+    apt-get install unzip -y
+fi
+
+
 curl -fsSL https://get.docker.com/ -o get-docker.sh
 sh get-docker.sh
 
